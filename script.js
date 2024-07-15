1132fa26d12fa9a9533092354e32fab3144c3c09
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-07-15 17:02:50 +0300

    step 14

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
 
-const dollarRegex = /dollar/i;
+const dollarRegex = /dollars/i;
 
-const denyList = [helpRegex];
+const denyList = [helpRegex, dollarRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
